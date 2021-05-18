6dfff785fe3683bd65cfb30443dfe2b8e31d1eaf
Wa-dot <wandrille.guiraud@student.yncrea.fr>
2021-05-18 20:57:35 +0200

    Mise en place du tri des éléments et commencement de la détection.

---
+++ combination.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wandr on 06/05/2021.
+//
+
+#include "combination.h"
---
+++ combination.h
@@ -0,0 +1,8 @@
+//
+// Created by wandr on 06/05/2021.
+//
+
+#ifndef POKER_COMBINATION_H
+#define POKER_COMBINATION_H
+
+#endif //POKER_COMBINATION_H
